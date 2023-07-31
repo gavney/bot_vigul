6f6982da7af4ecfb876fb60337ee66864714251a
Андрей <65560474+gavney@users.noreply.github.com>
2023-07-31 15:44:35 +0300

    Add files via upload

---
+++ bot/keyboard_config.py
@@ -0,0 +1,343 @@
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor 
+
+keyboard_create = VkKeyboard(one_time=False, inline=False)
+
+keyboard_create.add_button(
+    label="Начать",
+    color=VkKeyboardColor.PRIMARY,
+    payload={"type": "answer_create"},
+)
+
+
+keyboard_restart = VkKeyboard(one_time=False, inline=False)
+
+keyboard_restart.add_button(
+    label="Использовать сохранённую анкету",
+    color=VkKeyboardColor.POSITIVE,
+    payload={"type": "answer_create"},
+)
+
+keyboard_restart.add_line()
+
+keyboard_restart.add_button(
+    label="Заполнить заново",
+    color=VkKeyboardColor.PRIMARY,
+    payload={"type": "answer_create"},
+)
+
+
+keyboard_answer_4 = VkKeyboard(one_time=False, inline=False)
+
+keyboard_answer_4.add_button(
+    label="Мужской",
+    color=VkKeyboardColor.POSITIVE,
+    payload={"type": "answer_create"},
+)
+
+keyboard_answer_4.add_button(
+    label="Женский",
+    color=VkKeyboardColor.POSITIVE,
+    payload={"type": "answer_create"},
+)
+
+keyboard_answer_4.add_line()
+
+keyboard_answer_4.add_callback_button(
+    label="Назад",
+    color=VkKeyboardColor.PRIMARY,
+    payload={"type": "answer_change_back"},
+)
+
+
+keyboard_change = VkKeyboard(one_time=False, inline=False)
+
+keyboard_change.add_callback_button(
+    label="Назад",
+    color=VkKeyboardColor.SECONDARY,
+    payload={"type": "answer_change_back"},
+)
+
+
+
+keyboard_answer = VkKeyboard(one_time=False, inline=False)
+
+keyboard_answer.add_button(
+    label="Нет",
+    color=VkKeyboardColor.PRIMARY,
+    payload={"type": "answer_NO"},
+)
+
+keyboard_answer.add_button(
+    label="Да",
+    color=VkKeyboardColor.POSITIVE,
+    payload={"type": "answer_YES"},
+)
+
+keyboard_answer.add_line()
+
+keyboard_answer.add_callback_button(
+    label="Назад",
+    color=VkKeyboardColor.PRIMARY,
+    payload={"type": "answer_change_back"},
+)
+
+
+
+keyboard_answer_12 = VkKeyboard(one_time=False, inline=False)
+
+keyboard_answer_12.add_button(
+    label="Нет",
+    color=VkKeyboardColor.PRIMARY,
+    payload={"type": "answer_NO"},
+)
+
+keyboard_answer_12.add_button(
+    label="Да",
+    color=VkKeyboardColor.POSITIVE,
+    payload={"type": "answer_YES"},
+)
+
+keyboard_answer_12.add_line()
+
+keyboard_answer_12.add_button(
+    label="Кусается",
+    color=VkKeyboardColor.PRIMARY,
+    payload={"type": "answer_YES"},
+)
+
+keyboard_answer_12.add_line()
+
+keyboard_answer_12.add_callback_button(
+    label="Назад",
+    color=VkKeyboardColor.PRIMARY,
+    payload={"type": "answer_change_back"},
+)
+
+
+
+
+keyboard_answer_10 = VkKeyboard(one_time=False, inline=False)
+
+keyboard_answer_10.add_button(
+    label="Нет",
+    color=VkKeyboardColor.PRIMARY,
+    payload={"type": "answer_NO"},
+)
+
+keyboard_answer_10.add_button(
+    label="Иногда тянет",
+    color=VkKeyboardColor.POSITIVE,
+    payload={"type": "answer_YES"},
+)
+
+keyboard_answer_10.add_line()
+
+keyboard_answer_10.add_button(
+    label="Сильно тянет",
+    color=VkKeyboardColor.POSITIVE,
+    payload={"type": "answer_YES"},
+)
+
+keyboard_answer_10.add_line()
+
+keyboard_answer_10.add_callback_button(
+    label="Назад",
+    color=VkKeyboardColor.PRIMARY,
+    payload={"type": "answer_change_back"},
+)
+
+
+
+keyboard_answer_11 = VkKeyboard(one_time=False, inline=False)
+
+keyboard_answer_11.add_button(
+    label="Нет",
+    color=VkKeyboardColor.PRIMARY,
+    payload={"type": "answer_NO"},
+)
+
+keyboard_answer_11.add_button(
+    label="Иногда подбирает",
+    color=VkKeyboardColor.POSITIVE,
+    payload={"type": "answer_YES"},
+)
+
+keyboard_answer_11.add_line()
+
+keyboard_answer_11.add_button(
+    label="Часто подбирает",
+    color=VkKeyboardColor.POSITIVE,
+    payload={"type": "answer_YES"},
+)
+
+keyboard_answer_11.add_line()
+
+keyboard_answer_11.add_callback_button(
+    label="Назад",
+    color=VkKeyboardColor.PRIMARY,
+    payload={"type": "answer_change_back"},
+)
+
+
+keyboard_skip = VkKeyboard(one_time=False, inline=False)
+
+keyboard_skip.add_callback_button(
+    label="Назад",
+    color=VkKeyboardColor.PRIMARY,
+    payload={"type": "answer_change_back"},
+)
+
+
+
+keyboard_answer_21 = VkKeyboard(one_time=False, inline=False)
+
+keyboard_answer_21.add_button(
+    label="30 минут - 400 рублей",
+    color=VkKeyboardColor.POSITIVE,
+    payload={"type": "answer_NO"},
+)
+
+keyboard_answer_21.add_button(
+    label="1 час - 600 рублей",
+    color=VkKeyboardColor.POSITIVE,
+    payload={"type": "answer_YES"},
+)
+
+keyboard_answer_21.add_line()
+
+keyboard_answer_21.add_button(
+    label="1,5 часа - 800 рублей",
+    color=VkKeyboardColor.POSITIVE,
+    payload={"type": "answer_YES"},
+)
+
+keyboard_answer_21.add_line()
+
+keyboard_answer_21.add_callback_button(
+    label="Назад",
+    color=VkKeyboardColor.PRIMARY,
+    payload={"type": "answer_change_back"},
+)
+
+
+
+
+keyboard_answer_23 = VkKeyboard(one_time=False, inline=False)
+
+keyboard_answer_23.add_button(
+    label="На первом пробном выгуле",
+    color=VkKeyboardColor.POSITIVE,
+    payload={"type": "answer_NO"},
+)
+
+keyboard_answer_23.add_line()
+
+keyboard_answer_23.add_button(
+    label="Хочу связаться до первого выгула",
+    color=VkKeyboardColor.POSITIVE,
+    payload={"type": "answer_YES"},
+)
+
+keyboard_answer_23.add_line()
+
+keyboard_answer_23.add_button(
+    label="Знакомство не требуется",
+    color=VkKeyboardColor.PRIMARY,
+    payload={"type": "answer_YES"},
+)
+
+keyboard_answer_23.add_line()
+
+keyboard_answer_23.add_callback_button(
+    label="Назад",
+    color=VkKeyboardColor.PRIMARY,
+    payload={"type": "answer_change_back"},
+)
+
+
+
+
+keyboard_answer_24 = VkKeyboard(one_time=False, inline=False)
+
+keyboard_answer_24.add_button(
+    label="Дома встретят",
+    color=VkKeyboardColor.SECONDARY,
+    payload={"type": "answer_NO"},
+)
+
+keyboard_answer_24.add_line()
+
+keyboard_answer_24.add_button(
+    label="Ключи будут в тайном месте",
+    color=VkKeyboardColor.SECONDARY,
+    payload={"type": "answer_YES"},
+)
+
+keyboard_answer_24.add_line()
+
+keyboard_answer_24.add_button(
+    label="Передам ключи выгульщику",
+    color=VkKeyboardColor.SECONDARY,
+    payload={"type": "answer_YES"},
+)
+
+keyboard_answer_24.add_line()
+
+keyboard_answer_24.add_button(
+    label="Ключи уже у вас",
+    color=VkKeyboardColor.SECONDARY,
+    payload={"type": "answer_YES"},
+)
+
+keyboard_answer_24.add_line()
+
+keyboard_answer_24.add_callback_button(
+    label="Назад",
+    color=VkKeyboardColor.PRIMARY,
+    payload={"type": "answer_change_back"},
+)
+
+
+keyboard_answer_29 = VkKeyboard(one_time=False, inline=False)
+
+keyboard_answer_29.add_button(
+    label="В любое время",
+    color=VkKeyboardColor.POSITIVE,
+    payload={"type": "answer_NO"},
+)
+
+keyboard_answer_29.add_line()
+
+keyboard_answer_29.add_callback_button(
+    label="Назад",
+    color=VkKeyboardColor.PRIMARY,
+    payload={"type": "answer_change_back"},
+)
+
+keyboard_answer_result = VkKeyboard(one_time=True, inline=False)
+
+keyboard_answer_result.add_callback_button(
+    label="Закончить",
+    color=VkKeyboardColor.POSITIVE,
+    payload={"type": "result"},
+)
+
+keyboard_answer_21 = VkKeyboard(one_time=True, inline=False)
+
+keyboard_answer_21.add_button(
+    label="Не нужно",
+    color=VkKeyboardColor.POSITIVE,
+    payload={"type": "result"},
+)
+
+keyboard_answer_21.add_line()
+
+keyboard_answer_21.add_callback_button(
+    label="Назад",
+    color=VkKeyboardColor.PRIMARY,
+    payload={"type": "answer_change_back"},
+)
+
+
+
+
---
+++ bot/main.py
@@ -0,0 +1,115 @@
+import json
+
+from vk_api import VkApi
+from vk_api.utils import get_random_id
+from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
+import keyboard_config
+from questions import questions_data
+
+GROUP_ID = "221826865"
+GROUP_TOKEN = ""
+API_VERSION = "5.120"
+
+
+def save_data(data):
+    print(data)
+    json_object = json.dumps(data)
+ 
+    with open("users.json", "w") as outfile:
+        outfile.write(json_object)
+
+
+def open_data():
+    with open('users.json') as json_file:
+        data = json.load(json_file)
+        return data
+
+
+users = open_data()
+
+
+def main():
+    vk_session = VkApi(token=GROUP_TOKEN, api_version=API_VERSION)
+    vk = vk_session.get_api()
+    longpoll = VkBotLongPoll(vk_session, group_id=GROUP_ID)
+
+    try:
+        for event in longpoll.listen():
+                if event.type == VkBotEventType.MESSAGE_NEW and event.from_user:
+                    user_id = str(event.obj.message["from_id"])
+                    
+                    if user_id not in users:
+                        vk.messages.send(
+                            random_id=get_random_id(),
+                            peer_id=user_id,
+                            keyboard=keyboard_config.keyboard_create.get_keyboard(),
+                            message="Напишите 'начать', чтобы заказать выгул"
+                        )
+                        users.update({user_id: 0})
+
+                    if event.message.text.lower() == "начать":
+                        if users.get(user_id) == 0:
+                            users.update({user_id: 1})
+
+                        if users.get(user_id) >= len(questions_data):
+                            send_message(vk, user_id, "Анкета сохранена.", keyboard_config.keyboard_restart, step=0)
+
+                        if users.get(user_id) > 2 and users.get(user_id) < len(questions_data):
+                            send_message(vk, user_id, "Вы уже проходите.", keyboard_config.keyboard_skip, step=0)
+
+                    if event.message.text.lower() == "заполнить заново":
+                        users.update({user_id: 1})
+
+                    if event.message.text.lower() == "использовать сохранённую анкету": 
+                        users.update({user_id: 26})                                
+                        
+                    questions = questions_data.get(users.get(user_id))
+
+                    if questions:
+                        send_message(vk, user_id, questions[0], questions[1])  
+                                
+                elif event.type == VkBotEventType.MESSAGE_EVENT:
+                    user_id = str(event.object.user_id)
+
+                    vk.messages.sendMessageEventAnswer(
+                        event_id=event.object.event_id,
+                        user_id=event.object.user_id,
+                        peer_id=event.object.peer_id
+                    )
+
+                    if event.object.payload.get("type") == "answer_change_back":    
+                        if users.get(user_id) > 1:
+                            questions = questions_data.get(users.get(user_id) - 2)
+
+                            if questions:
+                                send_message(vk, user_id, questions[0], questions[1], -1)  
+                    
+                    
+                    if event.object.payload.get("type") == "result":    
+                        vk.messages.send(
+                            random_id=get_random_id(),
+                            peer_id=event.object.user_id,
+                            keyboard=VkKeyboard.get_empty_keyboard(),
+                            message="До скорых прогулок ;)"
+                        )
+                save_data(users)
+    except:
+        print("ошибка")
+
+
+
+def send_message(vk, peer_id, text, keyboard, step = 1):
+    if users.get(peer_id) != 0:
+    
+        users.update({peer_id: users.get(peer_id) + step})
+
+        vk.messages.send(
+            random_id=get_random_id(),
+            peer_id=peer_id,
+            keyboard=keyboard.get_keyboard(),
+            message=text
+        )
+
+if __name__ == "__main__":
+    main()
